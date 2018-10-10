c013e654c0767feb8a3edae19004735580ef675c
JTVictor <jtownsend@outlook.com>
2018-10-10 11:17:43 +0100

    code sperated into functions. One to draw ball, the other to move it

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
 var x = canvas.width/2;
 var y = canvas.height-30;
-
 var dx = 2;
 var dy = -2;
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y ,10, Math.PI*2, false);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
   x += dx;
   y += dy;
 }
+
 setInterval(draw, 10);
