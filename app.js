33f645e5be6821f74ebb400b3ea88ea2c7c1dde5
JTVictor <jtownsend@outlook.com>
2018-10-10 11:48:31 +0100

    Key event listeners added

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 
+var rightPressed = false;
+var leftPressed = false;
+
 var ballRadius = 10;
 
 function drawBall() {
@@ -41,5 +44,28 @@ function draw() {
   x += dx;
   y += dy;
 }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if(e.keyCode == 39) {
+    rightPressed = true;
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+ if(e.keyCode == 39) {
+   rightPressed = false;
+ }
+ else if(e.keyCode == 37) {
+   leftPressed = false;
+ }
+}
+
+
 
 setInterval(draw, 10);
